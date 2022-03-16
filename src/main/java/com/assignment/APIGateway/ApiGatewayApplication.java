2a49a275cf4cc1b5a6c462cd925bfb25c99f5c77
HEMNATH-PC\Hemnath <hemnath.j1995@gmail.com>
2022-03-16 12:17:42 +0530

    first commit

---
+++ src/main/java/com/assignment/APIGateway/ApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.assignment.APIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Gateway for Hotel Room Booking applicaiton
+ *
+ * @author Hemnath Jaishankar
+ */
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
